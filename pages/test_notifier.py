6f1d60cbecf39b98c9a3e5ba97e1f97266e44da0
jagadeshaidev-lab <jagadesh.ai.dev@gmail.com>
2025-11-14 20:56:11 +0530

    Update test_notifier.py

--- pages/test_notifier.py
+++ pages/test_notifier.py
@@ -10,7 +10,7 @@ sys.path.append(str(pathlib.Path(__file__).parent.parent))
 
 # --- STEP A: PASTE YOUR REAL SECRETS HERE ---
 os.environ['TWILIO_ACCOUNT_SID'] = "AC383e2107cac16a848fcd411b8e599f0c"
-os.environ['TWILIO_AUTH_TOKEN'] = "YOUR_REAL_AUTH_TOKEN_HERE" # Nee real token ikkada pettu
+os.environ['TWILIO_AUTH_TOKEN'] = "4697aebd9d7b2cebe2181645f63f918b" # Nee real token ikkada pettu
 os.environ['TWILIO_FROM_NUMBER'] = "whatsapp:+14155238886"
 os.environ['YOUR_WHATSAPP_NUMBER'] = "whatsapp:+919154625353"
 # NOTE: Firebase creds are not needed for this test
